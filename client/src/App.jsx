8fa15c5d9e75a6921fbd826eb81f3699081f3270
Harmeet Singh <128402172+hskhanduja03@users.noreply.github.com>
2025-07-23 11:58:09 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,19 +7,18 @@ import ShiningButton from "./ShiningButton";
 function App() {
   return (
     <BrowserRouter>
-      <header className="w-full px-4 sm:px-4 py-4 flex justify-between items-center bg-white border-b border-b-[#e6ebf4]">
-  <Link to="/">
-    <img 
-      src={logo2} 
-      className="h-10 w-auto object-contain" 
-      alt="logo" 
-    />
-  </Link>
+      <header className="w-full px-4 sm:px-4 py-4 flex justify-between items-center bg-white border-b-[#e6ebf4]">
+        <Link to={"/"}>
+          <img src={logo2} className="w-28 object-contain" alt="logo" />
+        </Link>
 
-  <Link to="/create-post">
-    <ShiningButton name="Create" />
-  </Link>
-</header>
+        <Link
+          // className="bg-violet-500 text-white px-4 py-2 rounded-md"
+          to={"/create-post"}
+        >
+          <ShiningButton name = {"Create"}/>
+        </Link>
+      </header>
 
         
       <main className="bg-[#f8fafe] sm:px-8 px-4 py-5 w-full min-h-[calc(100vh-73px) text-black">
